872006addcb8fe32afc4be0c952fc0fd421454e1
yagob <yagobeng2@gmail.com>
2026-05-17 22:12:35 +0300

    revert: back to working thum.io solution

--- src/services/puppeteer.js
+++ src/services/puppeteer.js
@@ -1,16 +1,12 @@
 const https = require('https');
 
 async function captureChart({ symbol, market, timeframe }) {
-  const tfMap = {
-    '1m':'1m','3m':'3m','5m':'5m','15m':'15m','30m':'30m',
-    '1h':'1h','2h':'2h','4h':'4h','1d':'1D','1w':'1W'
-  };
-  const interval = tfMap[timeframe] || '4h';
-
-  const url = `https://api.chart-img.com/v1/tradingview/advanced-chart?symbol=${market}%3A${symbol}&interval=${interval}&theme=dark&width=1200&height=600`;
+  const tvSymbol = `${market}:${symbol}`;
+  const tvUrl = `https://www.tradingview.com/chart/?symbol=${tvSymbol}`;
+  const thumbUrl = `https://image.thum.io/get/width/1200/crop/600/noanimate/wait/5/${tvUrl}`;
 
   return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
+    https.get(thumbUrl, (res) => {
       const chunks = [];
       res.on('data', chunk => chunks.push(chunk));
       res.on('end', () => resolve(Buffer.concat(chunks)));
